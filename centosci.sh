78d0aad0d87666e9a02550b2b7e87f423794fe53
Dusty Mabe <dusty@dustymabe.com>
2017-04-11 16:23:57 -0400

    update centosci instructions

--- centosci.sh
+++ centosci.sh
@@ -2,6 +2,8 @@
 set -eux -o pipefail
 
 yum install -y epel-release
-yum install -y ansible 
+yum install -y ansible git
+git clone https://github.com/dustymabe/tmp-20170227.git
+
 
 ansible-playbook -i localhost, -c local playbook.yml
